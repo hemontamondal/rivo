6126565626b4d61b5a24c894b2bf3a4fb740aa19
Hemonta Mondal <86686198+hemontamondal@users.noreply.github.com>
2023-10-11 16:11:54 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// js start
+ 
+
+
+// jq start
+$(document).ready(function(){
+
+	// mixitup
+	var mixer = mixitup('#mainTab');
+	var mixer = mixitup('#mainTab', {
+	   
+	    animation: {
+	        duration: 300
+	    },
+	    animation: {
+        easing: 'ease-in-out'
+    	}
+	});
+
+// lightbox
+
+  lightbox.option({
+      'resizeDuration': 300,
+      'wrapAround': true
+    });
+
+
+})
